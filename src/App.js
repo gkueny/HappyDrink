32d97e659e8279edc0a02b72a89830dbebee9fc5
gkueny <gaetankueny@gmail.com>
2016-11-26 12:43:45 +0100

    add css folder and move css into it + update App css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import logo                 from './logo.svg'
-import './App.css'
+import './css/App.css'
 
 import { etablishments }     from './etablishments/fixtures'
 
