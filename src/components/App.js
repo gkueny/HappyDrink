0b69a4b3b12696e88aa281bc81eb6eb2ab9d43ab
gkueny <gaetankueny@gmail.com>
2016-11-30 19:42:10 +0100

    little update

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react'
 import logo                 from '../assets/logo.svg'
 import '../css/App.css'
 
+import { establishments }    from './establishments/fixtures'
 import Establishment         from './establishments/Establishment'
-
-import { establishments }     from './establishments/fixtures'
 
 class App extends Component {
 
-    //On définit le state de notre component
-    state = {
-        pseudo  : "Inconnu"
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            pseudo  : "Inconnu"
+        }
+
     }
 
     randomPseudo = () => {
+
         let randomPseudo    = ""
         const possible      = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-        const size          = Math.floor(Math.random() * (11-5) ) + 5
+        const size          = Math.floor(Math.random() * 10) + 5
 
         for( let i=0; i < size; i++ )
             randomPseudo += possible.charAt(Math.floor(Math.random() * possible.length))
 
-        //On met à jour le state via la fonction setState
         this.setState({
             pseudo : randomPseudo
         })
@@ -44,7 +48,9 @@ class App extends Component {
 
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
+
                     <h2>Welcome "{ this.state.pseudo }" to { this.props.title }</h2>
+
                 </div>
 
                 <div className="App-intro">
